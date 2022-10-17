63f8324deb0fe9b3ff23dfc931147d8b6f33184e
Rick Vermeil <rickv85@gmail.com>
2022-10-17 17:16:40 -0600

    Finish Magician

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -6,7 +6,26 @@ class Magician {
     // Used logical OR operator to assign if magObj.favoriteAccessory
     // existed or 'top hat' if not. Surprised this worked!
     // Could not define in the constructor parameters as its brought in via an object.
-    this.favoriteAccessory = (magObj.clothing || `top hat`)
+    this.favoriteAccessory = (magObj.clothing || `top hat`);
+    this.confidencePercentage = 10;
+    }
+    performIncantation(says) {
+        return `${says.toUpperCase()}!`
+    }
+    performTrick() {
+        this.confidencePercentage += 10;
+        if (this.favoriteAccessory === "cape"){
+            return `PULL DOVE FROM SLEEVE`
+        } else {
+        return `PULL RABBIT FROM TOP HAT`
+        }
+    }
+    performShowStopper() {
+        if (this.confidencePercentage >= 100 && this.assistant) {
+            return `WOW! The magician totally just sawed that person in half!`
+        } else {
+            return `Oh no, this trick is not ready!`;
+        }
     }
 }
 
