3a418bfa39ec8c60c7c784f39d5dbfe2ca794b66
Rick Vermeil <rickv85@gmail.com>
2022-10-17 18:58:38 -0600

    Complete Medusa, Person, Statue

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,21 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+    constructor(name) {
+    this.name = name;
+    this.statues = [];
+    }
+    gazeAtVictim (victim) {
+        if (this.statues.length < 3) {
+            this.statues.push(new Statue(victim.name));
+        } else if (this.statues.length >= 3) {
+            this.statues.push(new Statue(victim.name));
+            const freed = this.statues.shift();
+            return new Person(freed.name, freed.mood = `relieved`);
+        }
+    }
+};
+
+
+module.exports = Medusa;
