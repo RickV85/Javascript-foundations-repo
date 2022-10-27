b2bac322b85b0eb19db9e8b0317b81c80b7560d2
Rick Vermeil <rickv85@gmail.com>
2022-10-27 10:16:59 -0600

    Complete Crafting

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,22 @@
 class Craft {
-
+    constructor(craftObj) {
+        this.name = craftObj.type;
+        this.materials = craftObj.materials;
+        this.completed = false;
+    }
+    completeCraft() {
+        this.completed = true;
+        return `All done! It looks great!`
+    }
+    calculateProjectTotal() {
+        // Make sure to create the var total above the for loop and then
+        // use the += addition assignment on it in the loop, return after the loop
+        var totalCost = 0;
+        for (let index = 0; index < this.materials.length; index++) {
+            totalCost += (this.materials[index].price * this.materials[index].amount);
+        }
+        return totalCost;
+    }
 }
 
 module.exports = Craft;
