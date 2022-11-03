f905b54214be035dc1f9dd2edfe174c1486dd24e
Rick Vermeil <rickv85@gmail.com>
2022-11-03 11:29:37 -0600

    Complete Adopt a dog

--- adopt-a-dog/src/dog.js
+++ adopt-a-dog/src/dog.js
@@ -1,8 +1,44 @@
 class Dog {
-  constructor() {
-
+  constructor(dogObj) {
+    this.name = dogObj.name;
+    this.age = dogObj.age;
+    this.energyLevel = 5;
+    this.hungry = true;
+    this.friends = [];
   }
-
+  eat() {
+    if (this.hungry === true){
+      this.hungry = false;
+      return `Yum!`;
+    } else if (this.hungry === false) {
+      return `I refuse to eat.`
+    }
+  }
+  fetchBall() {
+    // When invoked, this should reduce energyLevel by 1
+    // and return a string of This is fun!
+    // Then, if the energey level is less than or equal to 3
+    // do not reduce energyLevel and return `Nah...`
+    if (this.energyLevel > 3) {
+      this.energyLevel -= 1;
+      return `This is fun!`;
+    } else if (this.energyLevel <= 3) {
+      return `Nah, I'm going to sleep instead.`
+    }
+  }
+  sleep() {
+    // Add 1 to energyLevel unless its >= 10
+    if (this.energyLevel < 10) {
+      this.energyLevel += 1;
+    } else if (this.energyLevel >= 10) {
+      return `I have too much energy to rest. I'm going to chew something instead.`
+    }
+  }
+  makeNewFriend(friend) {
+    // Need to push a string of the friend argument's name
+    // in to the friends array
+    this.friends.push(friend.name);
+  }
 }
 
 module.exports = Dog;
--- adopt-a-dog/src/person.js
+++ adopt-a-dog/src/person.js
@@ -1,10 +1,42 @@
 var Dog = require("./dog.js");
 
 class Person {
-  constructor() {
+  constructor(name, dog) {
+    this.name = name;
+    this.dog = dog;
+  }
+  fillFoodBowl() {
+    // this.dog.hungry = false; also works, but this invokes a
+    // method of the Dog class instead
+    this.dog.eat();
+  }
+  throwBall() {
+    // if (this.dog.energyLevel <= 3) {
+    //   return;
+    // }
+    // this.dog.energyLevel -= 1;
+    // ^^^ this works but can also invoke a method from Dog class
+    // that will achieve the same effect.
+    this.dog.fetchBall();
+    return `${this.dog.name} loves playing fetch!`;
+  }
+  introduceNewFriends(dogObj) {
+    // this.dog.friends.push(dogObj.name);
+    // ^^^ this works but below invokes a method in Dog
+    this.dog.makeNewFriend(dogObj);
+  }
+  adoptAPup(newDogName, newDogAge) {
+    if (this.dog === undefined) {
+      // var newDog = new Dog({"name": name, "age": age})
+      // this.dog = newDog;
+      var newDog = new Dog({ name: newDogName, age: newDogAge });
+      this.dog = newDog;
+    } else {
+      return `You can't adopt ${newDogName}. You already have ${this.dog.name}!`
+    }
+    
 
   }
-
 }
 
 module.exports = Person;
