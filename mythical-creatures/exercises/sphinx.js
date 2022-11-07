35ccf0d6d7c226203687f38a35585e256eda04ae
Rick Vermeil <rickv85@gmail.com>
2022-11-07 07:30:42 -0700

    Complete Sphinx

--- mythical-creatures/exercises/sphinx.js
+++ mythical-creatures/exercises/sphinx.js
@@ -1,6 +1,7 @@
 class Sphinx {
     constructor() {
         this.riddles = [];
+        this.heroesEaten = 0;
     }
     collectRiddle(riddle) {
         if (this.riddles.length < 3) {
@@ -9,6 +10,18 @@ class Sphinx {
             this.riddles.push(riddle);
             this.riddles.splice(0,1);
         }
+    }
+    attemptAnswer(attempt) {
+        for (let index = 0; index < this.riddles.length; index++) {
+            if ((this.riddles[index].answer == attempt) && (this.riddles.length > 1)) {
+                this.riddles.splice(index, 1);
+                return `That wasn't that hard, I bet you don't get the next one`;
+            } else if ((this.riddles[index].answer == attempt) && (this.riddles.length <= 1)) {
+                this.riddles.splice(index, 1);
+                return `PSSSSSSS THIS HAS NEVER HAPPENED, HOW DID YOU KNOW THE ANSWER WAS "${attempt}"???`;
+            }
+        }
+        this.heroesEaten += 1;
     }
 }
 
