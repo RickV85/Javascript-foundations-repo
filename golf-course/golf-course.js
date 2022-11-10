eea6a04fed08020873a6e95c36758663a2a4e678
Rick Vermeil <rickv85@gmail.com>
2022-11-10 14:51:56 -0700

    Complete Golf Course

--- golf-course/golf-course.js
+++ golf-course/golf-course.js
@@ -1,6 +1,21 @@
 class GolfCourse {
-
-
+    constructor(name, difficulty, openings, features) {
+        this.name = name;
+        this.difficulty = difficulty;
+        this.openings = openings;
+        this.features = features;
+        this.currentlyPlaying = [];
+    }
+    checkInGroup(group) {
+        if (this.openings - (group.length + this.currentlyPlaying.length) >= 0) {
+            this.openings -= group.length;
+            for (var index = 0; index < group.length; index++) {
+                this.currentlyPlaying.unshift(group[index].name);
+                }
+            return 'You\'re checked in. Have fun!';
+        } else {
+            return 'Sorry, we are currently booked! Please come back later.';
+        }
+    }
 }
-
 module.exports = GolfCourse;
